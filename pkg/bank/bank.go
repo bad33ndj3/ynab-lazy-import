d4fd87a699de8ae0e643819452a96f9c500725ab
bad33ndj3 <casper.s@live.nl>
2020-10-22 20:35:26 +0200

    refactor: remove the unlogical methods

--- pkg/bank/bank.go
+++ pkg/bank/bank.go
@@ -31,7 +31,6 @@ func GetBank(bankType string) Bank {
 }
 
 func ReadDir(path string, account Account) ([]transaction.PayloadTransaction, error) {
-	lines := GetBank(account.Bank)
 	files, err := dirutil.FilePathWalkDir(path, ".csv")
 	if err != nil {
 		return nil, err
@@ -39,7 +38,7 @@ func ReadDir(path string, account Account) ([]transaction.PayloadTransaction, er
 
 	var transactions []transaction.PayloadTransaction
 	for _, file := range files {
-		lines, err := ReadFile(file, account.Iban, account.Account, lines)
+		lines, err := Read(file, account)
 		if err != nil {
 			return nil, err
 		}
@@ -49,44 +48,36 @@ func ReadDir(path string, account Account) ([]transaction.PayloadTransaction, er
 	return transactions, nil
 }
 
-func ReadFile(path, iban, accountID string, lines Bank) ([]transaction.PayloadTransaction, error) {
-	err := Read(lines, path, iban)
+func Read(path string, account Account) ([]transaction.PayloadTransaction, error) {
+	lines := GetBank(account.Bank)
+	if !lines.CorrectFile(path, account.Iban) {
+		return nil, nil
+	}
+
+	file, err := os.OpenFile(path, os.O_RDONLY, os.ModePerm)
 	if err != nil {
 		return nil, err
 	}
 
-	transactions, err := lines.ToYNAB(accountID)
+	gocsv.SetCSVReader(func(in io.Reader) gocsv.CSVReader {
+		r := csv.NewReader(in)
+		r.Comma = lines.Seperator()
+		return r // Allows use pipe as delimiter
+	})
+
+	if err := gocsv.UnmarshalFile(file, lines); err != nil {
+		return nil, err
+	}
+
+	err = file.Close()
+	if err != nil {
+		return nil, err
+	}
+
+	transactions, err := lines.ToYNAB(account.Account)
 	if err != nil {
 		return nil, err
 	}
 
 	return transactions, nil
-}
-
-func Read(out Bank, path, iban string) error {
-	if !out.CorrectFile(path, iban) {
-		return nil
-	}
-
-	file, err := os.OpenFile(path, os.O_RDONLY, os.ModePerm)
-	if err != nil {
-		return err
-	}
-
-	gocsv.SetCSVReader(func(in io.Reader) gocsv.CSVReader {
-		r := csv.NewReader(in)
-		r.Comma = out.Seperator()
-		return r // Allows use pipe as delimiter
-	})
-
-	if err := gocsv.UnmarshalFile(file, out); err != nil {
-		return err
-	}
-
-	err = file.Close()
-	if err != nil {
-		return err
-	}
-
-	return nil
 }
